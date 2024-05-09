b3fd22c91144babc77f58b8be8da109e64686218
Ilmal Sabda Fathir <isaffathir@outlook.com>
2024-05-09 10:04:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'click==8.1.3',
-        "repath",
+        "repath"
     ],
     entry_points= {
         'console_scripts': 
